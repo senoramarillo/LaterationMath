5845db4099cc7a4a14e3474780e0671154cf761e
Andy <Andy@lenovo>
2017-09-14 12:58:18 +0200

    - added *.mat files

--- LaterationMath/src/latmath/test/MatlabGeolaterationNTest.java
+++ LaterationMath/src/latmath/test/MatlabGeolaterationNTest.java
@@ -2,13 +2,10 @@ package latmath.test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
-
 import com.jmatio.io.MatFileReader;
 import com.jmatio.types.MLDouble;
-
 import latmath.util.Point2d;
 
 public class MatlabGeolaterationNTest {
@@ -50,10 +47,9 @@ public class MatlabGeolaterationNTest {
 		Point2d[] anchors = new Point2d[] { new Point2d(24.5626016260160, 2.29970731707320),
 				new Point2d(23.1605118679050, 8.57622125902990), new Point2d(24.7228648090820, 12.1175545923630),
 				new Point2d(26.0127689243310, 9.01743205528970), new Point2d(30.8588226401950, 9.19865358007020),
-				new Point2d(30.6951496849370, 1.20326677881980), new Point2d(26.5792387026600, 1.27245974100970),
-				new Point2d(24.9928307764210, 6.69500716668635), new Point2d(27.0096672540500, 6.66041068559140) };
+				new Point2d(30.6951496849370, 1.20326677881980), new Point2d(26.5792387026600, 1.27245974100970) };
 
-		MatFileReader matfilereader = new MatFileReader("distance.mat");
+		MatFileReader matfilereader = new MatFileReader("distance_7_398.mat");
 		double[][] mlArrayDouble = ((MLDouble) matfilereader.getMLArray("distance_to_anchor")).getArray();
 		Point2d estimate;
 		double[] temp;
